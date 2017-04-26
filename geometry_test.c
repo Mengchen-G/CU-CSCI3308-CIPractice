f9ccb9c48f89933b2984062206703f3b5ffcce99
Mengchen-G <mego6698@colorado.edu>
2017-04-26 14:27:43 -0600

    Update geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -179,7 +179,7 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
     
-    TCase* tc_2d_area = tcase_create("coord_2d_area_triangle");
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
     tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
 
     /* Add Cases to Suite */
